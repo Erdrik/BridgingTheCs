1f7695a6103ffe35fd4ec33530985c4090bcfab5
Samuel Ballard-Adams <s.ballard-adams@outlook.com>
2019-03-20 22:38:31 +0000

    Rectangle uses RectangleBridge!

---
+++ BridgingTheCs/ShowMeSomething/CPPWrapper/Rectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowMeSomething.CPPWrapper
+{
+    public class Rectangle : IDisposable
+    {
+        private IntPtr nativeObject;
+
+        public Rectangle()
+        {
+            this.nativeObject = RectangleBridge.ConstructorRectangle();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public void SetPosition(float x, float y)
+        {
+            RectangleBridge.SetPosition(this.nativeObject, x, y);
+        }
+
+        public void SetDimensions(float width, float height)
+        {
+            RectangleBridge.SetDimensions(this.nativeObject, width, height);
+        }
+
+        public bool PointIsInside(float x, float y)
+        {
+            return RectangleBridge.PointIsInside(this.nativeObject, x, y);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.nativeObject != IntPtr.Zero)
+            {
+                RectangleBridge.DestructorRectangle(this.nativeObject);
+                this.nativeObject = IntPtr.Zero;
+            }
+
+            if (disposing)
+            {
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        ~Rectangle()
+        {
+            Dispose(false);
+        }
+    }
+}
---
+++ BridgingTheCs/ShowMeSomething/CPPWrapper/RectangleBridge.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ShowMeSomething.CPPWrapper
+{
+    public class RectangleBridge
+    {
+        [DllImport("TheBridge.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool Exists();
+
+        [DllImport("TheBridge.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr ConstructorRectangle();
+
+        [DllImport("TheBridge.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DestructorRectangle(IntPtr rectangle);
+
+        [DllImport("TheBridge.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetPosition(IntPtr rectangle, float x, float y);
+
+        [DllImport("TheBridge.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void SetDimensions(IntPtr rectangle, float width, float height);
+
+        [DllImport("TheBridge.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool PointIsInside(IntPtr rectangle, float x, float y);
+    }
+}
